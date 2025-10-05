e3ece6a86e596d7e4091da152dab7fa71f75dbf0
jsrayanyaqoob <rayanyaqoob83@gmail.com>
2025-10-05 13:40:15 +0500

    node js practice done

--- index.js
+++ index.js
@@ -2,5 +2,27 @@
 // console.log(randomNum)
 
 
-const rayan = require("./second")
-console.log(rayan.isDeveloper)
+// const rayan = require("./second")
+// console.log(rayan.isDeveloper)
+
+
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path')
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,"home.html"))
+})
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname,"contact.html"))
+})
+
+app.get('/service', (req, res) => {
+    res.send('<h1>Hello World</h1>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
